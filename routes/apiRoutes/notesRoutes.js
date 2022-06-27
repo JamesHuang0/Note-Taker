912118ac95ddd393af569123e5fb344d5f001d94
mrxanthic <mrxanthic@gmail.com>
2022-06-27 00:17:11 -0500

    added server routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -6,3 +6,16 @@ router.get('/notes', (req, res) => {
     let saved = notes;
     res.json(saved);
 })
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = addNote(req.body, notes);
+    res.json(note);
+})
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
+
+module.exports = router;
